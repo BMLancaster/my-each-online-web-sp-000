6e60435d0206a4a614b28b293c6c256196f65647
Benjamin M Lancaster <bmlancaster07@gmail.com>
2018-12-07 20:37:26 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  if block_given?
+    i = 0
+ 
+    while i < collection.length
+      yield(collection[i])
+      puts i
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
